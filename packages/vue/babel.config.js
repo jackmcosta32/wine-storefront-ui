3d8bb46598e96bec5a1d272c5cd409dfe1c85714
joaocosta <joaomcosta32@gmail.com>
2021-08-28 10:45:55 -0300

    [fix] Babel plugin proposal class, property and methods must be loose

--- packages/vue/babel.config.js
+++ packages/vue/babel.config.js
@@ -2,5 +2,7 @@ module.exports = {
   presets: ["@vue/app"],
   plugins: [
     ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
   ],
 };
